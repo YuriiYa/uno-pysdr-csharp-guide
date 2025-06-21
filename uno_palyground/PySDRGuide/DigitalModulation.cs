d99f6b015b109f4d95f5e13d5da4f69537c6298c
yurii <argongalen@gmail.com>
2025-06-21 22:00:05 +0300

    DigitalModulation examples added phase noise

--- uno_palyground/PySDRGuide/DigitalModulation.cs
+++ uno_palyground/PySDRGuide/DigitalModulation.cs
@@ -21,29 +21,30 @@ public class DigitalModulation
         var img = Numpy.np.sin(x_radians);
         var real = Numpy.np.cos(x_radians);
 
-        var qsig = new Complex[x_symbols.len];
-        
-        //var phase_noise = Numpy.np.random.randn(x_symbols.len) * 0.1; //adjust multiplier for "strength" of phase noise
-        //var dd = x_symbols * Numpy.np.exp(1j * phase_noise);
-        
-        for (var i = 0; i < x_symbols.len; i++)
-        {
-            qsig[i] = new Complex(real.GetData<double>()[i], img.GetData<double>()[i]);
-        }
-        //var r = Numpy.np.array(sig);
-        //var x_symbols = Numpy.np.cos(x_radians) + new Complex(0, 1) * Numpy.np.sin(x_radians); // this produces our QPSK complex symbols
-        var rsig = new Complex[num_symbols];
-        var nsigq = Numpy.np.random.randn(num_symbols);
-        var nsigi = Numpy.np.random.randn(num_symbols);
+        var signal = new Complex[x_symbols.len];
         // AWGN with unity power
         var noise_power = 0.1;
+        var phase_noise = Numpy.np.random.randn(x_symbols.len) * noise_power; //adjust multiplier for "strength" of phase noise
+                                                                              //var dd = x_symbols * Numpy.np.exp(1j * phase_noise);
+        var iqphase_noise = new Complex[x_symbols.len];
+        for (var i = 0; i < x_symbols.len; i++)
+        {
+            iqphase_noise[i] = new Complex(0, phase_noise.GetData<double>()[i]);
+            signal[i] = new Complex(real.GetData<double>()[i], img.GetData<double>()[i]);
+        }
+        var npiq_phase_noise = Numpy.np.exp(Numpy.np.array(iqphase_noise));
+        //var r = Numpy.np.array(sig);
+        //var x_symbols = Numpy.np.cos(x_radians) + new Complex(0, 1) * Numpy.np.sin(x_radians); // this produces our QPSK complex symbols
+        var guasian_noise = new Complex[num_symbols];
+        var nsigq = Numpy.np.random.randn(num_symbols);
+        var nsigi = Numpy.np.random.randn(num_symbols);
 
         for (var i = 0; i < num_symbols; i++)
         {
-            rsig[i] = qsig[i] + (new Complex(nsigq.GetData<double>()[i], nsigi.GetData<double>()[i]) * Math.Sqrt(noise_power) / Math.Sqrt(2));
+            guasian_noise[i] = new Complex(nsigq.GetData<double>()[i], nsigi.GetData<double>()[i]) * Math.Sqrt(noise_power) / Math.Sqrt(2);
         }
 
-        var sig = Numpy.np.array(rsig);
+        var sig = Numpy.np.array(signal) * npiq_phase_noise + Numpy.np.array(guasian_noise);
         var x = sig.real.GetData<double>();
         var y = sig.imag.GetData<double>();
         var hm = _plot.Add.Markers(x, y);
